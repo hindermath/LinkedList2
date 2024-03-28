150f5bd46f46779e3f124f715370b3820e4d1f8e
Thorsten Hindermann <hindermath@googlemail.com>
2024-03-28 16:17:20 +0100

    Add LibLinkedList2Test project and initial test
    A new test project, LibLinkedList2Test, has been created with an initial placeholder test method. This project is configured with the necessary dependencies and references, and has been added to the solution. Future unit tests for the LibLinkedList2 project should be placed in this test project.

---
+++ LibLinkedList2Test/LinkedListTest.cs
@@ -0,0 +1,15 @@
+using LibLinkedList2;
+
+namespace LibLinkedList2Test;
+
+[TestClass]
+public class LinkedListTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
