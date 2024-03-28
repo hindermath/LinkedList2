285c6e0c5ae7f0307f39f57a56cf5957b6ad991d
Thorsten Hindermann <hindermath@googlemail.com>
2024-03-28 16:34:59 +0100

    Add generic LinkedList tests
    Two new methods are added in the LinkedListTest.cs file for testing Generic LinkedList for integer and string types. The tests ensure that the added value at the head is the expected value.

--- LibLinkedList2Test/LinkedListTest.cs
+++ LibLinkedList2Test/LinkedListTest.cs
@@ -19,3 +19,35 @@ public class LinkedListTest
         if (list.Head != null) Assert.AreEqual(value, list.Head.Value);
     }
 }
+
+[TestClass]
+public class GenLinkedListTest
+{
+    [TestMethod]
+    [DataRow(1)]
+    [DataRow(2)]
+    [DataRow(3)]
+    public void GenLinkedListIntTestMethod(int value)
+    {
+        // Arrange
+        GenLinkedList<int> genListInt = new GenLinkedList<int>();
+        // Act
+        genListInt.AddNode(value);
+        // Assert
+        if (genListInt.Head != null) Assert.AreEqual(value, genListInt.Head.Value);
+    }
+
+    [TestMethod]
+    [DataRow("one")]
+    [DataRow("two")]
+    [DataRow("three")]
+    public void GenLinkedListStrTestMethod(string value)
+    {
+        // Arrange
+        GenLinkedList<string> genListStr = new GenLinkedList<string>();
+        // Act
+        genListStr.AddNode(value);
+        // Assert
+        if (genListStr.Head != null) Assert.AreEqual(value, genListStr.Head.Value);
+    }
+}
