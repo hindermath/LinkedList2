bc88beadbd19e0a48300d7082d28153a230cdaf4
Thorsten Hindermann <hindermath@googlemail.com>
2024-04-01 19:52:45 +0200

    Update test methods in LinkedListTest
    The test methods in LinkedListTest.cs have been updated with new DataRow inputs. These updates include the use of negative and positive numbers as well as empty strings. This adjustment will allow the tests to further evaluate the linked list's behavior under various conditions.

--- LibLinkedList2Test/LinkedListTest.cs
+++ LibLinkedList2Test/LinkedListTest.cs
@@ -7,8 +7,8 @@ public class LinkedListTest
 {
     [TestMethod]
     [DataRow(1)]
-    [DataRow(2)]
-    [DataRow(3)]
+    [DataRow(-2)]
+    [DataRow(+3)]
     public void LinkedListSingleNodeTestMethod(int value)
     {
         // Arrange
@@ -19,9 +19,9 @@ public class LinkedListTest
         if (list.Head != null) Assert.AreEqual(value, list.Head.Value);
     }
     [TestMethod]
-    [DataRow(1, 2, 3)]
-    [DataRow(4, 5, 6)]
-    [DataRow(7, 8, 9)]
+    [DataRow(-1, 2, +3)]
+    [DataRow(4, -5, +6)]
+    [DataRow(+7, 8, -9)]
     public void LinkedListMultipleNodesTestMethod(int value1, int value2, int value3)
     {
         // Arrange
@@ -45,25 +45,26 @@ public class GenLinkedListTest
 {
     [TestMethod]
     [DataRow(1)]
-    [DataRow(2)]
+    [DataRow(-2)]
     [DataRow(3)]
+    [DataRow(0)]
     public void GenLinkedListIntSingleNodeTestMethod(int value)
     {
         SingleNodeTestMethod<int>(value);
     }
     [TestMethod]
-    [DataRow(1, 2, 3)]
-    [DataRow(4, 5, 6)]
-    [DataRow(7, 8, 9)]
+    [DataRow(-1, 2, 3)]
+    [DataRow(4, -5, 6)]
+    [DataRow(7, 8, -9)]
     public void GenLinkedListIntMultipleNodesTestMethod(int value1, int value2, int value3)
     {
         MultipleNodesTestMethod<int>(value1, value2, value3);
     }
 
     [TestMethod]
-    [DataRow(new double[] {1, 2, 3})]
-    [DataRow(new double[] {4, 5, 6})]
-    [DataRow(new double[] {7, 8, 9})]
+    [DataRow(new double[] {1, 2.0, 3.4})]
+    [DataRow(new double[] {-4, 5, -6.1})]
+    [DataRow(new double[] {7.0, -8.97, 9})]
     public void GenLinkedListDblArrayMultipleNodesTestMethod(double[] values)
     {
         ArrayMultipleNodesTestMethod<double>(values);
@@ -73,6 +74,7 @@ public class GenLinkedListTest
     [DataRow("one")]
     [DataRow("two")]
     [DataRow("three")]
+    [DataRow("")]
     public void GenLinkedListStringMultipleNodesTestMethod(string value)
     {
         SingleNodeTestMethod<string>(value);
@@ -81,9 +83,21 @@ public class GenLinkedListTest
     [DataRow("one", "two", "three")]
     [DataRow("four", "five", "six")]
     [DataRow("seven", "eight", "nine")]
+    [DataRow("", "", "")]
+    [DataRow("one", "", "nine")]
     public void GenLinkedListStringMultipleNodesTestMethod(string value1, string value2, string value3)
     {
         MultipleNodesTestMethod<string>(value1, value2, value3);
+    }
+    [TestMethod]
+    [DataRow(new string[] {"one", "two", "three"})]
+    [DataRow(new string[] {"four", "five", "six"})]
+    [DataRow(new string[] {"seven", "eight", "nine"})]
+    [DataRow(new string[] {"", "", ""})]
+    [DataRow(new string[] {"one", "", "nine"})]
+    public void GenLinkedListStringArrayMultipleNodesTestMethod(string[] values)
+    {
+        ArrayMultipleNodesTestMethod<string>(values);
     }
 
     private void SingleNodeTestMethod<T>(T value)
