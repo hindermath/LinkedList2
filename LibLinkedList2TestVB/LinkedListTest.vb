4047c80a641e41e7e908250389cad6208db392fe
Thorsten Hindermann <thorsten.hindermann@gwdg.de>
2024-04-02 17:33:02 +0200

    Refactor LinkedList tests and add new tests
    The test class in the LinkedList Test file was renamed to 'SimpleLinkedListTestVb' and the test methods have been elaborated, adding 'LinkedList' to their names. Also, a new test method called 'LinkedListMultipleNodesTestSub' was added to accommodate tests with multiple data rows. A 'GenLinkedListTestVb' test class was created to handle general linked list tests.

--- LibLinkedList2TestVB/LinkedListTest.vb
+++ LibLinkedList2TestVB/LinkedListTest.vb
@@ -3,18 +3,58 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 Namespace LibLinkedList2TestVB
     <TestClass>
-    Public Class LinkedListTest
+    Public Class SimpleLinkedListTestVb
         <TestMethod>
         <DataRow(1)>
         <DataRow(-2)>
         <DataRow(+3)>
-        Sub TestSub(value As Integer)
+        Sub LinkedListSingleNodeTestSub(value As Integer)
             REM Arrange
             DIM list AS SimpleLinkedList = NEW SimpleLinkedList()
             REM Act
             list.AddNode(value)
             REM Assert
             Assert.AreEqual(value, list.Head.Value)
+        End Sub
+        <TestMethod>
+        <DataRow(-1, 2, +3)>
+        <DataRow(4, -5, +6)>
+        <DataRow(+7, 8, -9)>
+        Sub LinkedListMultipleNodesTestSub(value1 As Integer, value2 As Integer, value3 As Integer)
+            REM Arrange
+            DIM list AS SimpleLinkedList = NEW SimpleLinkedList()
+            REM Act
+            list.AddNode(value1)
+            list.AddNode(value2)
+            list.AddNode(value3)
+            REM Assert
+            Assert.AreEqual(value1, list.Head.Value)
+            Assert.AreEqual(value2, list.Head.Next.Value)
+            Assert.AreEqual(value3, list.Head.Next.Next.Value)
+        End Sub
+    End Class
+
+    <TestClass>
+    Public Class GenLinkedListTestVb
+        <TestMethod>
+        <DataRow(1)>
+        <DataRow(-2)>
+        <DataRow(+3)>
+        <DataRow(0)>
+        Sub GenLinkedListIntSingleNodeTestSub(value As Integer)
+            SingleNodeTestSub(Of Integer)(value)
+        End Sub
+
+        Private Sub SingleNodeTestSub(Of T)(value As T)
+            REM Arrange
+            DIM genList AS GenLinkedList(Of T) = NEW GenLinkedList(Of T)()
+            REM Act
+            genList.AddNode(value)
+            REM Assert
+            Assert.isNotNull(genList.Head, "Head node is null.")
+            If genList.Head isNot Nothing Then
+                Assert.AreEqual(value, genList.Head.Value, "Head node value is incorrect.")
+            End If
         End Sub
     End Class
 End Namespace
