64db5716d7cb0837875aaaf2b66e674224dd4832
Thorsten Hindermann <thorsten.hindermann@gwdg.de>
2024-04-09 15:17:54 +0200

    Refactor list printing methods in LinkedList program
    The previous individual list printing methods have been replaced with two more generic print functions for IntegerLinkedLists and GenericLinkedLists. This refactoring not only reduces duplicates but also makes the code more maintainable. Furthermore, it allows for smoother outputs generation and debugging.

--- LinkedList2/Program.cs
+++ LinkedList2/Program.cs
@@ -14,19 +14,59 @@ var debugWriter = TextWriter.Synchronized(Debugger.IsAttached ? new StringWriter
 list.AddNode(1);
 list.AddNode(2);
 list.AddNode(3);
-list.PrintList();
+PrintList(list, Console.Out);
 
 genericListInt.AddNode(1);
 genericListInt.AddNode(2);
 genericListInt.AddNode(3);
-genericListInt.PrintList(Console.Out);
-genericListInt.PrintList(fileWriter);
-genericListInt.PrintList(stringWriter);
+PrintGenericList<int>(genericListInt, Console.Out);
+PrintGenericList<int>(genericListInt, fileWriter);
+PrintGenericList<int>(genericListInt, stringWriter);
 var listAsString = stringWriter.ToString();
-genericListInt.PrintList(debugWriter);
+PrintGenericList<int>(genericListInt, debugWriter);
 var debugOutput = debugWriter.ToString();
 
 genericListStr.AddNode("one");
 genericListStr.AddNode("two");
 genericListStr.AddNode("three");
-genericListStr.PrintList(Console.Out);
+PrintGenericList<string>(genericListStr, Console.Out);
+PrintGenericList<string>(genericListStr, fileWriter);
+PrintGenericList<string>(genericListStr, stringWriter);
+listAsString = stringWriter.ToString();
+PrintGenericList<string>(genericListStr, debugWriter);
+debugOutput = debugWriter.ToString();
+
+return;
+
+void PrintGenericList<T>(GenericLinkedList<T> list, TextWriter writer)
+{
+    writer.WriteLine("Start of LinkedList print out:");
+
+    var counter = 0;
+    var current = list.Head;
+    while (current != null)
+    {
+        writer.WriteLine($"Node at index {counter} has value of type {current.Value.GetType()} and value: {current.Value}");
+        current = current.Next;
+        counter++;
+    }
+
+    writer.WriteLine($"End of LinkedList printout. The linked list has {list.Count} nodes in total.");
+}
+
+void PrintList(IntegerLinkedList list, TextWriter writer)
+{
+    writer.WriteLine("Start of LinkedList print out:");
+
+    var counter = 0;
+    var current = list.Head;
+    while (current != null)
+    {
+        writer.WriteLine($"Node at index {counter} has value of type {current.Value.GetType()} and value: {current.Value}");
+        current = current.Next;
+        counter++;
+    }
+
+    writer.WriteLine($"End of LinkedList printout. The linked list has {list.Count} nodes in total.");
+
+}
