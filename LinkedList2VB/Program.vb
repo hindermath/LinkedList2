810224e1a02442b1a1765a00350c4bb79bb80f6d
Thorsten Hindermann <thorsten.hindermann@gwdg.de>
2024-04-11 09:35:58 +0200

    Add output to file and string for LinkedList
    In addition to outputting to the console, LinkedList results are now also written to a file and a string. This change will allow the output to be stored for future use and manipulation. Both generic and non-generic LinkedLists are supported.

--- LinkedList2VB/Program.vb
+++ LinkedList2VB/Program.vb
@@ -7,19 +7,31 @@ Module Program
         DIM genericListInt AS GenericLinkedList(Of Integer) = NEW GenericLinkedList(Of Integer)()
         DIM genericListStr AS GenericLinkedList(Of String) = NEW GenericLinkedList(Of String)()
 
+        DIM fileWriter AS System.Io.StreamWriter = NEW System.IO.StreamWriter("output.txt")
+        Dim stringWriter As System.IO.StringWriter = New System.IO.StringWriter()
+
+
         list.AddNode(1)
         list.AddNode(2)
         list.AddNode(3)
         list.PrintList(Console.Out)
+        list.PrintList(fileWriter)
+        list.PrintList(stringWriter)
 
         genericListInt.AddNode(1)
         genericListInt.AddNode(2)
         genericListInt.AddNode(3)
         genericListInt.PrintList(Console.Out)
+        genericListInt.PrintList(fileWriter)
+        genericListInt.PrintList(stringWriter)
+        DIM listAsString AS String = stringWriter.ToString()
 
         genericListStr.AddNode("one")
         genericListStr.AddNode("two")
         genericListStr.AddNode("three")
         genericListStr.PrintList(Console.Out)
+        genericListStr.PrintList(fileWriter)
+        genericListStr.PrintList(stringWriter)
+        listAsString = stringWriter.ToString()
     End Sub
 End Module
