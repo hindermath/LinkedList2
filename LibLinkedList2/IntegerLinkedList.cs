48da7a8b30aa46ace9b446dc0d91b33678119829
Thorsten Hindermann <hindermath@googlemail.com>
2024-04-14 15:31:06 +0200

    Add method to retrieve item at specific index in LinkedList
    This commit implements the 'ItemAt' method in the IntegerLinkedList class. This method retrieves the item at a specified index in the linked list, throwing an exception if the index is out of range or negative. This allows for easier and safer access to specific elements in the list.

--- LibLinkedList2/IntegerLinkedList.cs
+++ LibLinkedList2/IntegerLinkedList.cs
@@ -30,4 +30,29 @@ public class IntegerLinkedList
         }
         Count++;
     }
+
+    public int ImtemAt(int index)
+    {
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index must not be negative");
+        }
+
+        if (First is null)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index was too large for the list");
+        }
+
+        var currentNode = First;
+        for (int i = 0; i < index; i++)
+        {
+            currentNode = currentNode.Next;
+
+            if (currentNode is null)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index was too large for the list");
+            }
+        }
+        return currentNode.Value;
+    }
 }
